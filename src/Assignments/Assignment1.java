7856af38b5f429d669b55f5e855fea3d27280c86
Imteyaj I Mujavar <imteyaj.mujavar1992@gmail.com>
2025-11-03 13:46:54 +0530

    Assignment1

---
+++ src/Assignments/Assignment1.java
@@ -0,0 +1,99 @@
+package Assignments;
+
+import java.sql.Time;
+
+public class Assignment1 {
+
+	private static final char A = 0;
+
+	public static void main(String[] args) {
+		// 1. Temperature of a city in degrees Celsius: 25.5
+		float Temprature = 25.5f;
+		System.out.println("1st Ans -> Temperature of a city in degrees Celsius:" + Temprature);
+		
+		//2. Whether a customer has placed an order: true or false
+		
+		
+		//3. Person's phone number: "123-456-7890"
+		int PhoneNumber = 1234567890;
+		System.out.println("3rd Ans -> Person's phone number:" + PhoneNumber);
+		
+		
+		//4. Amount of money in a customer's bank account: 1000.50
+		float AmountOfMoney = 1000.50f;
+		System.out.println("4th Ans -> Amount of money in a customer's bank account: " + AmountOfMoney);
+		
+		//5. Person's email address: "john.doe@example.com"		
+		String  PersonsEmailAddress = "john.doe@example.com";
+		System.out.println("5th Ans -> Person's email address: "+PersonsEmailAddress);
+		
+		
+		//6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194	
+		double latitude =37.7749;
+		double longitude = -122.4194;
+		System.out.println("6th Ans -> latitude of the Location is: "+ latitude + " And longitude of the Location is: " +longitude);
+		
+	
+		//7. Person's marital status: true or false
+		
+		
+		//8. Person's occupation: "Software Engineer"	
+		String  occupation = "Software Engineer";
+		System.out.println("8th Ans -> Person's occupation: " +occupation);
+		
+		//9. Person's favourite colour: "Blue"
+		String  favouriteColour = "Blue";
+		System.out.println("9th Ans -> Person's favourite colour: " +favouriteColour);
+		
+		
+		//10.Current year: 2023
+		int CurrentYear = 2023;
+		System.out.println("10th Ans -> Current year:" + CurrentYear);
+	
+	
+		//11.Number of followers on a social media platform: 1,000,000
+		int followers = 1000000;
+		System.out.println("11th Ans -> Number of followers on a social media platform:" + followers);
+	
+		
+		//12.Rating of a movie: 7.5
+		float Rating = 7.5f;
+		System.out.println("12th Ans -> Rating of a movie:" + Rating);
+		
+		//13.Person's blood type: 'A'
+		String bloodType = "A";
+		System.out.println("13th Ans -> Person's blood type:" + bloodType);
+		
+		//14.Title of a book: "To Kill a Mockingbird"
+		String  TitleOfABook = "To Kill a Mockingbird";
+		System.out.println("14th Ans -> Title of a book: " +TitleOfABook);
+		
+		//15.Number of employees in a company: 500
+		int NumberOfEmployees = 500;
+		System.out.println("15th Ans -> Number of employees in a company:" + NumberOfEmployees);
+	
+		//16.Time of an event: 2:30 PM
+		String  TimeOfAnEvent = "2:30 PM";
+		System.out.println("16th Ans -> Time of an event: " +TimeOfAnEvent);
+		
+		
+		//17.Name of a country: "United States"
+		String  NameOfACountry = "United States";
+		System.out.println("17th Ans -> Name of a country: "+NameOfACountry);
+		
+		//18.Person's eye color: "Brown"
+		String  PersonsEyeColor = "Brown";
+		System.out.println("18th Ans -> Person's eye color: " +PersonsEyeColor);
+		
+		//19.Person's birthplace: "New York City"
+		String  birthplace = "New York City";
+		System.out.println("19th Ans -> Person's birthplace: " +birthplace);
+		
+		//20.Distance between two cities: 200.5
+		float Distance = 200.5f;
+		System.out.println("20th Ans -> Distance between two cities:" + Distance);
+	
+		
+	}
+
+}
--- src/language/basics/HelloWorld.java
+++ src/language/basics/HelloWorld.java
@@ -5,6 +5,14 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		 System.out.println("Hello World");
 		 System.out.println("Imteyaj????????");
+		 
+		 // Benifits of print statements
+		 
+//       1. Education and learning purpose.
+//       2. Debugging and error detection.
+//       3. Monitoring the program execution.
+//       4. Testing and validating the results.
+//       5. Log the system results to analyze later.
 
 	}
 
